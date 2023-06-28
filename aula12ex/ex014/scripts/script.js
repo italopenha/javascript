3351117082a2c82c72fadfc542c615d36c442daa
Ítalo Penha <italopenha77@outlook.com>
2023-06-28 19:55:01 -0300

    style: Alterando cores e fontes

--- aula12ex/ex014/scripts/script.js
+++ aula12ex/ex014/scripts/script.js
@@ -1,6 +1,7 @@
 setInterval(carregar, 1000)
 
 function carregar() {
+    var titulo = document.querySelector("h1")
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
@@ -9,14 +10,24 @@ function carregar() {
     var segundos = String(data.getSeconds()).padStart(2, "0")
     msg.innerHTML = (`${horas}:${minutos}:${segundos}`)
 
-    if (horas >= 0 && horas < 12) {
+    if (horas >= 6 && horas < 12) {
         img.src = 'imgs/manha.jpg'
-        document.body.style.background = '#4585cc'
+        document.body.style.backgroundImage = 'linear-gradient(to bottom, #387ac4, #73a2d3)'
+        titulo.innerHTML = 'BOM DIA!'
+        titulo.style.fontFamily = 'Belanosima, sans-serif'
+        msg.style.fontFamily = 'Belanosima, sans-serif'
     } else if (horas >= 12 && horas < 18) {
         img.src = 'imgs/tarde.jpg'
-        document.body.style.background = '#fc9a3f'
+        document.body.style.backgroundImage = 'linear-gradient(to bottom, #f9eb00, #f18b44)'
+        titulo.innerHTML = 'BOA TARDE!'
+        titulo.style.color = 'black'
+        titulo.style.fontFamily = 'Concert One, cursive'
+        msg.style.fontFamily = 'Concert One, cursive'
     } else {
         img.src = 'imgs/noite.jpg'
-        document.body.style.background = '#112641'
+        document.body.style.backgroundImage = 'linear-gradient(to bottom, #08223e, #0e59b8)'
+        titulo.innerHTML = 'BOA NOITE!'
+        titulo.style.fontFamily = 'Play, sans-serif'
+        msg.style.fontFamily = 'Play, sans-serif'
     }
 }
